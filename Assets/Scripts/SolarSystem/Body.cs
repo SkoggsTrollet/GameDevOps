4c980902d1be184db4f7eb41ca1ba19f5dfd2da5
DannyDarwiche <darwichedanny@gmail.com>
2021-06-17 17:12:59 +0200

    Add 3 tests for Body
    Addressing #26
    Ran 3 tests on 2 new classes, Body and BodyTest.

---
+++ Assets/Scripts/SolarSystem/Body.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body
+{
+    public Vector3 initialVelocity;
+    public Vector3 currentVelocity;
+    public Vector3 position;
+    float mass;
+    float radius;
+    bool isSun;
+
+    public Body(bool isSun, Vector3 initialVelocity, Vector3 position, float surfaceGravity, float radius)
+    {
+        this.isSun = isSun;
+        this.initialVelocity = initialVelocity;
+        this.currentVelocity = initialVelocity;
+        this.position = position;
+        this.radius = radius;
+        mass = surfaceGravity * radius * radius / Universe.gravitationalConstant;
+    }
+
+    public void UpdateVelocity(Body[] bodies, float timeStep)
+    {
+        if (isSun)
+        {
+            return;
+        }
+
+        foreach (var body in bodies)
+        {
+            if (body != this)
+            {
+                float sqrDist = (body.position - body.position).sqrMagnitude;
+                Vector3 forceDir = (body.position - body.position).normalized;
+                Vector3 force = forceDir * Universe.gravitationalConstant * mass * body.mass / sqrDist;
+                Vector3 acceleration = force / mass;
+                currentVelocity += acceleration * timeStep;
+            }
+        }
+    }
+}
---
+++ Assets/Test/SolarSystem/BodyTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+public class BodyTest
+{
+    Body CreateTestBody(bool isSun)
+    {
+        return new Body(isSun, new Vector3(0, 0, 100), Vector3.zero, 274, 2000);
+    }
+
+    [Test]
+    public void no_change_in_velocity_for_sun()
+    {
+        Body sun = CreateTestBody(true);
+        Body[] bodies = { sun };
+
+        sun.UpdateVelocity(bodies, Universe.physicsTimeStep);
+        
+        Assert.AreEqual(sun.initialVelocity, sun.currentVelocity);
+    }
+
+    [Test]
+    public void no_change_in_velocity_for_sun_with_other_body()
+    {
+        Body sun = CreateTestBody(true);
+        Body body = CreateTestBody(false);
+        Body[] bodies = { sun, body };
+
+        sun.UpdateVelocity(bodies, Universe.physicsTimeStep);
+
+        Assert.AreEqual(sun.initialVelocity, sun.currentVelocity);
+    }
+
+    [Test]
+    public void change_in_velocity_for_body_with_other_body()
+    {
+        Body sun = CreateTestBody(true);
+        Body body = CreateTestBody(false);
+        Body[] bodies = { sun, body };
+
+        body.UpdateVelocity(bodies, Universe.physicsTimeStep);
+
+        Assert.AreNotEqual(body.initialVelocity, body.currentVelocity);
+    }
+}
