92ec9038025d669907a1c71b06031dc34584b5b1
DannyDarwiche <darwichedanny@gmail.com>
2021-06-21 10:48:58 +0200

    add 2 tests for HealthSystem
    WIP #28

--- Assets/Scripts/Entities/HealthSystem.cs
+++ Assets/Scripts/Entities/HealthSystem.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class HealthSystem
 {
+    int maxHitPoints;
     int hitPoints;
 
     public HealthSystem(int initialHitPoints)
     {
-        hitPoints = initialHitPoints;
+        if (initialHitPoints < 1)
+        {
+            maxHitPoints = hitPoints = 1;
+        }
+        else
+        {
+            maxHitPoints = hitPoints = initialHitPoints;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (damage < 0)
+        {
+            return;
+        }
+
+        hitPoints -= damage;
+        hitPoints = Mathf.Clamp(hitPoints, 0, maxHitPoints);
     }
 
     public int GetHitPoints()
--- Assets/Test/Entities/HealthSystemTest.cs
+++ Assets/Test/Entities/HealthSystemTest.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
 
 public class HealthSystemTest
 {
@@ -12,5 +8,26 @@ public class HealthSystemTest
         HealthSystem healthSystem = new HealthSystem(5);
 
         Assert.AreEqual(5, healthSystem.GetHitPoints());
+    }
+
+    [TestCase(5, 5)]
+    [TestCase(1, -2)]
+    public void contructed_with_correct_values(int expected, int hitPoints)
+    {
+        HealthSystem healthSystem = new HealthSystem(hitPoints);
+
+        Assert.AreEqual(expected, healthSystem.GetHitPoints());
+    }
+
+    [TestCase(4, 1)]
+    [TestCase(0, 6)]
+    [TestCase(5, -1)]
+    public void damaged_with_correct_values(int expected, int damage)
+    {
+        HealthSystem healthSystem = new HealthSystem(5);
+
+        healthSystem.TakeDamage(damage);
+
+        Assert.AreEqual(expected, healthSystem.GetHitPoints());
     }
 }
