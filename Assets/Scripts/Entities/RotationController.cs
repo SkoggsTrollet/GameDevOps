219dbd6bf56498e5fa6149819364cf68244a4df1
IsakN <isaq.tokyo@gmail.com>
2021-06-21 17:34:37 +0200

    Add 2 Tests
    WIP #32

--- Assets/Scripts/Entities/RotationController.cs
+++ Assets/Scripts/Entities/RotationController.cs
@@ -6,15 +6,65 @@ public class RotationController
     float yawSpeed; 
     float rollSpeed;
 
-    public RotationController(float pitchSpeed, float yawSpeed, float rollSpeed)
+    Vector2 innerLimit;
+    Vector2 outerLimit;
+
+    float pitchDelta;
+    float yawDelta;
+    float rollDelta;
+
+    Vector2 mouseDistance;
+
+    public RotationController(float pitchSpeed, float yawSpeed, float rollSpeed, Vector2 innerLimit, Vector2 outerLimit)
     {
         this.pitchSpeed = Mathf.Abs(pitchSpeed);
         this.yawSpeed = Mathf.Abs(yawSpeed);
         this.rollSpeed = Mathf.Abs(rollSpeed);
+
+        this.innerLimit = innerLimit;
+        this.outerLimit = outerLimit;
+    }
+
+    public void UpdateRotation(float direction, float timeStep)
+    {
+        rollDelta = direction * timeStep * rollSpeed;
+    }
+
+    public void CalculateMouseDistance(Vector2 mousePos)
+    {
+        mouseDistance = Vector2.zero;
+
+        if(mousePos.x > Screen.width / 2 + innerLimit.x)
+        {
+            mouseDistance.x = (mousePos.x - (Screen.width / 2 + innerLimit.x)) / (Screen.width / 2 - innerLimit.x);
+        }
+        else if(mousePos.x < Screen.width / 2 - innerLimit.x)
+        {
+            mouseDistance.x = (mousePos.x - (Screen.width / 2 - innerLimit.x)) / (Screen.width / 2 - innerLimit.x);
+        }
+
+        if(mousePos.y > Screen.height / 2 + innerLimit.y)
+        {
+            mouseDistance.y = (mousePos.y - (Screen.height / 2 + innerLimit.y)) / (Screen.height / 2 - innerLimit.y);
+        }
+        else if(mousePos.y < Screen.height / 2 - innerLimit.y)
+        {
+            mouseDistance.y = (mousePos.y - (Screen.height / 2 - innerLimit.y)) / (Screen.height / 2 - innerLimit.y);
+        }
     }
 
     public Vector3 GetRotationSpeed()
     {
         return new Vector3(pitchSpeed, yawSpeed, rollSpeed);
+    }
+
+    public Vector3 GetDeltaRotation()
+    {
+        return new Vector3(pitchDelta, yawDelta, rollDelta);
+    }
+
+    public Vector2 GetMouseDistance()
+    {
+        return mouseDistance;
     }
 }
--- Assets/Test/Entities/RotationControllerTest.cs
+++ Assets/Test/Entities/RotationControllerTest.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using UnityEngine;
 
 public class RotationControllerTest
 {
@@ -6,10 +7,63 @@ public class RotationControllerTest
     [TestCase(-10, -10, -10, 10)]
     public void rotation_speed_set_properly(float pitch, float yaw, float roll, float expected)
     {
-        RotationController rotationController = new RotationController(pitch, yaw, roll);
+        RotationController rotationController = new RotationController(pitch, yaw, roll, Vector2.zero, Vector2.zero);
 
         Assert.That(rotationController.GetRotationSpeed().x, Is.EqualTo(expected));
         Assert.That(rotationController.GetRotationSpeed().y, Is.EqualTo(expected));
         Assert.That(rotationController.GetRotationSpeed().z, Is.EqualTo(expected));
+    }
+
+    [TestCase(1, 1, 10, 10)]
+    [TestCase(-1, 1, 10, -10)]
+    public void roll_delta_calculated_properly(float direction, float timeStep, float roll, float expected)
+    {
+        RotationController rotationController = new RotationController(0, 0, roll, Vector2.zero, Vector2.zero);
+
+        rotationController.UpdateRotation(direction, timeStep);
+
+        Assert.That(rotationController.GetDeltaRotation().z, Is.EqualTo(expected));
+    }
+
+
+    // Using aspect ration Full HD (1920x1080) in unity
+    [TestCase(
+        1920/2+1,1080/2+1,
+        1,1,
+        0,0)]
+    [TestCase(
+        1920/2, 1080/2,
+        1,1,
+        0,0)]
+    [TestCase(
+        1920/4, 1080/2,
+        0,0,
+        -0.5f,0)]
+    [TestCase(
+        1920*3/4, 1080/2,
+        0,0,
+        0.5f,0)]
+    [TestCase(
+        1920/2, 1080/4,
+        0,0,
+        0,-0.5f)]
+    [TestCase(
+        1920/2, 1080*3/4,
+        0,0,
+        0,0.5f)]
+    public void calculate_mouse_position_properly(
+        float xMousePos, float yMousePos,
+        float xInnerLimit, float yInnerLimt,
+        float xExpected, float yExpected)
+    {
+        Vector2 mousePos = new Vector2(xMousePos, yMousePos);
+        Vector2 innerLimit = new Vector2(xInnerLimit, yInnerLimt);
+        Vector2 expected = new Vector2(xExpected, yExpected);
+
+        RotationController rotationController = new RotationController(0, 0, 0, innerLimit, Vector2.zero);
+
+        rotationController.CalculateMouseDistance(mousePos);
+
+        Assert.That(rotationController.GetMouseDistance(), Is.EqualTo(expected));
     }
 }
